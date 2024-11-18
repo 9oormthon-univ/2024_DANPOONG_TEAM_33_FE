ecfc100efb4d35be0fbefe9d9970752c213ab834
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-11-18 14:45:21 +0900

    feat: 소셜로그인 처리 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
     <Routes>
+      <Route path="/oauth2/login/kakao" element={<AuthPage />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
