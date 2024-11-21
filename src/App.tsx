0bc30fb761bbff731f00771627870fb89fb7725b
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-11-22 05:05:13 +0900

    feat: 메인페이지 구현 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import AuthPage from "./pages/AuthPage";
-
+import LoginPage from "./pages/LoginPage.tsx";
+import AuthPage from "./pages/AuthPage.tsx";
+import MainPage from "./pages/MainPage.tsx";
+import ResumePage from "./pages/ ResumePage.tsx";
+import MyPage from "./pages/MyPage.tsx";
+import OnBoardingPage from "./pages/OnBoardingPage.tsx";
 function App() {
   return (
     <Routes>
+      <Route path="/onboarding" element={<OnBoardingPage />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/resume" element={<ResumePage />} />
       <Route path="/oauth2/login/kakao" element={<AuthPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<MainPage />} />
     </Routes>
   );
 }
