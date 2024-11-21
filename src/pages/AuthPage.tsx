78c83543c8689bad97e8cecccd8f5772ebc17fe5
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-11-22 04:51:00 +0900

    feat: 사용자 정보 조회 기능 추가

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,28 +1,33 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import { getToken } from "../service/UserService.ts";
+import { getToken, getUserInfo } from "../service/UserService.ts"; // getUserInfo 추가
 
 const AuthPage = () => {
   const navigate = useNavigate();
+
   useEffect(() => {
     const authenticateUser = async () => {
+      const code = new URLSearchParams(window.location.search).get("code");
+
+      if (!code) return;
+
       try {
-        const code = new URLSearchParams(window.location.search).get("code");
-        console.log(code);
-        if (code) {
-          const response = await getToken(code);
-          if (response.success) {
+        const tokenResponse = await getToken(code);
+        if (tokenResponse.success) {
+          const userInfoResponse = await getUserInfo(); // 사용자 정보 가져오기
+          if (userInfoResponse.success) {
             navigate("/");
           }
         }
       } catch (error) {
-        console.log("로그인 실패", error);
+        console.error("로그인 실패:", error);
       }
     };
+
     authenticateUser();
   }, [navigate]);
 
-  return <div></div>;
+  return null;
 };
 
 export default AuthPage;
